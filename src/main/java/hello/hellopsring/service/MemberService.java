ded6998c7559b9b4300660226b26a247b6bfbdcf
찬익 박 <piaocanyi1997@gmail.com>
2023-07-02 14:20:49 +0900

    회원 서비스 개발

---
+++ src/main/java/hello/hellopsring/service/MemberService.java
@@ -0,0 +1,54 @@
+package hello.hellopsring.service;
+
+import hello.hellopsring.domain.Member;
+import hello.hellopsring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     * join할 경우 중복회원 검증 및 통과 & 저장
+     * @param member
+     * @return
+     */
+    public  Long join(Member member) {
+
+        //같은 이름이 있는 중복 회원은 X
+        //Optional로 감싸야 ifPresent 사용가능
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다");
+//        });
+
+        //위 Optional 코드 마이그레이션
+        validateDuplicateMember(member);  //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+
+    /**
+     * 전체회원 조회
+     * @return
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
